5d05083511461556e3ea426c434bea7f83848bbd
dhdmstoa <oeunsaem5@gmail.com>
2026-07-17 22:10:19 +0900

    feat:: 이미지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ type Photo = {
 }
 
 const PHOTOS: Photo[] = [
-  { id: 1, place: 'Schoolyard', date: '2026.05.14', uploader: 'Seoyeon, Class 3', semesterLabel: '2026-1' },
+  { id: 1, place: 'Schoolyard', date: '2026.05.14', uploader: 'Seoyeon, Class 3', semesterLabel: '2026-1', imageUrl: '/schoolyard.jpg' },
   { id: 2, place: 'Cafeteria', date: '2026.05.10', uploader: 'Doyoon, Class 1', semesterLabel: '2026-1' },
   { id: 3, place: 'Main Stairs', date: '2026.04.28', uploader: 'Haeun, Class 2', semesterLabel: '2026-1' },
   { id: 4, place: 'Music Room', date: '2025.11.02', uploader: 'Jiho, Class 4', semesterLabel: '2025-2' },
